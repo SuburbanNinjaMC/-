26e8ee81434185d0991f4426950987762fb825dd
SuburbanNinjaMC <suburbanninja575@gmail.com>
2016-06-05 15:32:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var
 worldTaunt = document.getElementById("worldTaunt"),
 name;
-worldTaunt.addEventListener(click, "sayHello");
 function sayHello(){
   name = prompt("What is your name");
   alert("Hey, " + name);
