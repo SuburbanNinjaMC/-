dd5dea82d793f1fde036e6ba2726dfe7a2214ab6
SuburbanNinjaMC <suburbanninja575@gmail.com>
2016-06-05 16:03:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,4 +17,15 @@ document.addEventListener('DOMContentLoaded', function () {
 if (Notification.permission !== "granted")
 Notification.requestPermission();
 })
-notification = new Notification('Hi');
+if(!Notification){
+  alert('Sorry, switch to Chromium for notifications... :P');
+}
+if (Notification.permission =='granted'){
+  new Notification('Bananas for sale', {
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Bananas_white_background_DS.jpg',
+    body: 'Bananas! $100 off';
+  })
+  notification.onclick=function(){
+    window.open('http://suburbanninjamc.github.io/-/');
+  };
+}
