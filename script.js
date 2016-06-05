2c511620bf9f8d7a5b45ac7b7376548f70e2dc23
SuburbanNinjaMC <suburbanninja575@gmail.com>
2016-06-05 15:42:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var
 worldTaunt = document.getElementById("worldTaunt"),
-name;
+name,
+notification;
 function sayHello(){
   name = prompt("What is your name");
   alert("Hey, " + name);
 }
+
+
+
+
+
+
+//Do not take away variable 'Notification' or the code below.
+notification = new Notification('Hi');
