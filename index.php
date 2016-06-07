aacc212745a536face189a145923f4ec89d69653
SuburbanNinjaMC <suburbanninja575@gmail.com>
2016-06-07 14:45:40 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="icon" type="image/png" sizes="16x16" href="/-/favicon-16x16.png">
+    <title>
+      SuburbanNinjaMC - Musician and YouTuber
+    </title>
+  </head>
+  <body id = "Body">
+    <!-- This is the header -->
+    <div id="fixedheader">
+      <div style = "float:right;">
+      <div class="dropdown">
+        <button class="dropbtn">YourName</button>
+          <div class="dropdown-content">
+            <a href="http://suburbanninjamc.github.io/-/profile/">Profile</a>
+            <a href="http://suburbanninjamc.github.io/-/settings/">Settings</a>
+            <a href="#">Messenger</a>
+            <a href="http://suburbanninjamc.github.io/-/otherlinks/">Other Links</a>
+          </div>
+        </div>
+    </div>
+    </div>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <br>
+    <br>
+    <br>
+    <img src="https://yt3.ggpht.com/-_pPk3dCgOZw/AAAAAAAAAAI/AAAAAAAAAAA/y4OcQjWcxAg/s900-c-k-no-rj-c0xffffff/photo.jpg" alt="PhotoImage" id = "PhotoImage" width = "300" height = "300">
+    <h1 id = "Title">SuburbanNinjaMC</h1>
+    <!-- <h2 d="worldTaunt" onclick="sayHello()">Click to say Hello World</h2> -->
+    <p id = "intro">Hello. I am SuburbanNinjaMC. Welcome to my third website. Here you will find music, links to my youtube videos, and more.
+    #subscribe for more.</p>
+    <!--Do not delete below!!!!!!!!! -->
+    <script src="script.js"></script>
+    
+    <a href="https://www.youtube.com/channel/UCMV7MuxUHWjc7M13QJz2Bbw" id = "MusicLabel" target=_blank>Music Label</a>
+    <a href="https://www.youtube.com/channel/UCEtE0WHjz1tjSSxIlxy2PpA" id = "YouTubeChannel" target=_blank>Main Channel</a>
+    <a href="http://aelon.x10.mx" id = "Aelon" target=_blank>Arkagaen</a>
+  </body>
+</html>
